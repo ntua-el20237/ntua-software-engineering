4ad2a472962147d23c64ab1ba9876e4826ae21b3
nektmpalos <131957984+nektmpalos@users.noreply.github.com>
2023-11-30 21:21:11 +0200

    Update run.py
    Basic front back connection

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import subprocess
 import os
 import requests #pip install requests
 import mysql.connector
+from flask import Flask, render_template,request
 
 #install python dependencies
 subprocess.check_call("pip install mysql-connector", shell=True)
@@ -22,7 +23,7 @@ cursor = conn.cursor()
 #Closing the connection
 conn.close()
 
-
+app=Flask(__name__)
 
 
 def search_movie(title):
@@ -40,13 +41,35 @@ def search_movie(title):
     else:
         return "Error fetching data."
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/search',methods=['POST'])
+def search():
+    if request.method == 'POST':
+        movie_title=request.form['movie_title']
+        result = search_movie(movie_title)
+        if isinstance(result,dict):
+            return render_template('result.html',result=result)
+        else:
+            return render_template('result.html',error=result)
+        
+
 # Usage example
-movie_title = input("Enter the movie title: ")
-result = search_movie(movie_title)
-if isinstance(result, dict):
-    print("Title:", result['Title'])
-    print("Year:", result['Year'])
-    print("IMDb Rating:", result['imdbRating'])
-    print("Plot:", result['Plot'])
-else:
-    print(result)
+#movie_title = input("Enter the movie title: ")
+#result = search_movie(movie_title)
+#if isinstance(result, dict):
+  #  print("Title:", result['Title'])
+  #  print("Year:", result['Year'])
+   # print("IMDb Rating:", result['imdbRating'])
+   # print("Plot:", result['Plot'])
+#else:
+ #   print(result)
+
+    
+if __name__ == '__main__':
+    app.run(debug=True, port=9876)
+
+
+
