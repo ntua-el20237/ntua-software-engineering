461028a13db55602862f9dd7a4e4769a98a01d2f
Despina Panagopoulou <despinapng@despinas-macbook-pro.home>
2023-12-06 19:52:46 +0200

    changes

--- run.py
+++ run.py
@@ -1,30 +1,25 @@
 import subprocess
 import os
-import requests #pip install requests
+import requests
 import mysql.connector
-from flask import Flask, render_template,request
+from flask import Flask, session, render_template, request, redirect, url_for, flash
 
 #install python dependencies
-subprocess.check_call("pip install mysql-connector", shell=True)
-subprocess.check_call('pip install -r requirements.txt', shell=True)
+#subprocess.check_call("pip install mysql-connector", shell=True)
+#subprocess.check_call('pip install -r requirements.txt', shell=True)
 
 print("Dependencies downloaded successfully........")
 
-
-
 #establishing the connection
 conn = mysql.connector.connect(
-   user='root', host='localhost', password='1234', port=3306)#change the password to your own password
+   user='root', host='localhost', password='1234', port=3306, database='ntuaflix')#change the password to your own password
 conn.autocommit = True
 
 #Creating a cursor object using the cursor() method
 cursor = conn.cursor()
 
-#Closing the connection
-conn.close()
-
 app=Flask(__name__)
-
+app.config['SECRET_KEY'] = 'mysecretkey'  # Set the secret key for Flask app
 
 def search_movie(title):
     base_url = "http://www.omdbapi.com/"
@@ -37,24 +32,58 @@ def search_movie(title):
         if movie_data['Response'] == 'True':
             return movie_data
         else:
-            return "Movie not found."
+            return "Movie not found"
     else:
-        return "Error fetching data."
+        return "Error fetching data"
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/search',methods=['POST'])
+@app.route('/search', methods=['POST'])
 def search():
     if request.method == 'POST':
-        movie_title=request.form['movie_title']
-        result = search_movie(movie_title)
-        if isinstance(result,dict):
-            return render_template('result.html',result=result)
-        else:
-            return render_template('result.html',error=result)
+        movie_title = request.form['movie_title']
+        return redirect(url_for('results', movie_title=movie_title))
+
+@app.route('/<movie_title>')
+def results(movie_title):
+    result = search_movie(movie_title)
+    if isinstance(result, dict):
+        return render_template('result.html', result=result)
+    else:
+        flash(result, 'danger')
+        return redirect(url_for('index'))
         
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        user_name = request.form.get("user_name")
+        submitted_user_password = request.form.get("submitted_user_password")
+        
+        cursor.execute("SELECT * FROM users WHERE user_name=%s AND user_password=%s", (user_name, submitted_user_password))
+        user = cursor.fetchone()
+        
+        if user:
+            session['user_name'] = user_name
+            session['user'] = user
+            return redirect(url_for('index'))
+        else:
+            flash("Incorrect username or password.", 'danger')
+            return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    else:
+        return redirect(url_for('index'))
+    
+@app.route('/chat')
+def chat():
+    return render_template('chat.html')
 
 # Usage example
 #movie_title = input("Enter the movie title: ")
