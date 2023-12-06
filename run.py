9b0a8c7af72d80aad8bba25d9f82610d3419c36c
Danae Spentzou <el20237@mail.ntua.gr>
2023-12-06 22:29:44 +0200

    changes

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ print("Dependencies downloaded successfully........")
 
 #establishing the connection
 conn = mysql.connector.connect(
-   user='root', host='localhost', password='1234', port=3306, database='ntuaflix')#change the password to your own password
+   user='root', host='localhost', password='pass', port=3306, database='ntuaflix')#change the password to your own password
 conn.autocommit = True
 
 #Creating a cursor object using the cursor() method
@@ -43,7 +43,9 @@ def index():
 @app.route('/search', methods=['POST'])
 def search():
     if request.method == 'POST':
+        movie_id = request.form['movie_id']
         movie_title = request.form['movie_title']
+        movie_genre = request.form['movie_genre']
         return redirect(url_for('results', movie_title=movie_title))
 
 @app.route('/<movie_title>')
